9701e68d2215e7d1d553b0558a886ff9ba4d0caf
Pushpraj Singh Chauhan <chauhanpushprajsingh003@gmail.com>
2024-02-17 19:00:46 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const age = 25;
-const name = "Pushpraj";
+const name1 = "Pushpraj";
 const dob = "13/05/2003";
 
 function display() {
-  console.log("My name is " + name);
+  console.log("My name is " + name1);
+}
+
+function isAbove18() {
+  return age >= 18;
 }
